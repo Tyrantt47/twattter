47061581c8d08541413999e24ac737be493db54d
Curtis <tyrantt47@hotmail.com>
2015-03-21 21:56:41 -0700

    Add user microposts

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Twattter"
   end
 
   test "should get help" do
@@ -16,12 +16,12 @@ class StaticPagesControllerTest < ActionController::TestCase
 test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "About | Twattter"
   end
 
     test "should get contact" do
     get :contact
     assert_response :success
-    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Contact | Twattter"
 end
 end
